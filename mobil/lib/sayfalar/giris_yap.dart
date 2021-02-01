099d86cd92d3ba49a5e842289a0bc70f2119b008
dershub <hubders@gmail.com>
2021-02-01 22:13:20 +0300

    1 Şubat oturumu

--- mobil/lib/sayfalar/giris_yap.dart
+++ mobil/lib/sayfalar/giris_yap.dart
@@ -1,9 +1,47 @@
 import 'package:auth_buttons/auth_buttons.dart';
-
-import '../gerecler/renkler.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 
-class GirisYap extends StatelessWidget {
+// Relative (göreceli) path
+import '../gerecler/renkler.dart';
+
+class GirisYap extends StatefulWidget {
+  @override
+  _GirisYapState createState() => _GirisYapState();
+}
+
+class _GirisYapState extends State<GirisYap> {
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> _googleIleGirisYap() async {
+    try {
+      final GoogleSignInAccount googleHesabi = await _googleSignIn.signIn();
+      /* final GoogleSignInAuthentication googleDogrulama =
+          await googleHesabi.authentication;
+
+      // Create a new credential
+      final GoogleAuthCredential kimlikBilgileri =
+          GoogleAuthProvider.credential(
+        accessToken: googleDogrulama.accessToken,
+        idToken: googleDogrulama.idToken,
+      );
+
+      try {
+        // Anonim hesaptan çıkış yap
+        await FirebaseAuth.instance.signOut();
+        // Google hesabı ile giriş yap
+        await FirebaseAuth.instance.signInWithCredential(kimlikBilgileri);
+
+        Navigator.popUntil(context, (route) => route.isFirst);
+      } catch (e) {
+        print(e);
+      } */
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,9 +71,7 @@ class GirisYap extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 16),
-                    GoogleAuthButton(
-                      onPressed: () {},
-                    ),
+                    GoogleAuthButton(onPressed: _googleIleGirisYap),
                     SizedBox(height: 16),
                     AppleAuthButton(
                       onPressed: () {},
